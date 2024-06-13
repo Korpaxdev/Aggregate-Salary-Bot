68a119973e0982fa9f2e28d8f0be913f5c4b205f
korpaxdev <korpaxdev@gmail.com>
2024-06-13 14:21:43 +0300

    Update: .gitignore, salary_service. Add: new pipeline in pipeline_utils

--- src/services/salary_service.py
+++ src/services/salary_service.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from services.database_service import DatabaseService
+from utils.pipeline_utils import get_date_pipeline
 from utils.type_utils import By
 
 
@@ -9,4 +10,4 @@ class SalaryService:
         self.__collection = database_service.db["sample_collection"]
 
     def get_salaries_by(self, dt_from: datetime, dt_upto: datetime, by: By):
-        pass
+        return self.__collection.aggregate(get_date_pipeline(dt_from, dt_upto, by))
--- src/utils/pipeline_utils.py
+++ src/utils/pipeline_utils.py
@@ -1,5 +1,33 @@
 from datetime import datetime
 
+from utils.type_utils import By
+
 
 def _base_date_pipeline(dt_from: datetime, dt_upto: datetime):
     return [{"$sort": {"dt": 1}}, {"$match": {"dt": {"$gte": dt_from, "$lte": dt_upto}}}]
+
+
+def by_month_pipeline(dt_from: datetime, dt_upto: datetime):
+    return [
+        *_base_date_pipeline(dt_from, dt_upto),
+        {
+            "$group": {
+                "_id": {
+                    "$dateFromParts": {
+                        "year": {"$year": "$dt"},
+                        "month": {"$month": "$dt"},
+                    }
+                },
+                "sum": {"$sum": "$value"},
+            }
+        },
+        {"$sort": {"_id": 1}},
+    ]
+
+
+def get_date_pipeline(dt_from: datetime, dt_upto: datetime, by: By):
+    match (by):
+        case "month":
+            return by_month_pipeline(dt_from, dt_upto)
+        case _:
+            raise NotImplementedError
