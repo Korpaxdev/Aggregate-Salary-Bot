4ae18bc3426ac312f6cd9a9810a04027c0879eb2
korpaxdev <korpaxdev@gmail.com>
2024-06-13 14:30:04 +0300

    Add: by_date_pipeline

--- src/utils/pipeline_utils.py
+++ src/utils/pipeline_utils.py
@@ -25,9 +25,34 @@ def by_month_pipeline(dt_from: datetime, dt_upto: datetime):
     ]
 
 
+def by_day_pipeline(dt_from: datetime, dt_upto: datetime):
+    return [
+        *_base_date_pipeline(dt_from, dt_upto),
+        {
+            "$group": {
+                "_id": {
+                    "$dateFromParts": {
+                        "year": {"$year": "$dt"},
+                        "month": {"$month": "$dt"},
+                        "day": {"$dayOfMonth": "$dt"},
+                    }
+                },
+                "sum": {"$sum": "$value"},
+            }
+        },
+        {
+            "$sort": {
+                "_id": 1,
+            }
+        },
+    ]
+
+
 def get_date_pipeline(dt_from: datetime, dt_upto: datetime, by: By):
     match (by):
         case "month":
             return by_month_pipeline(dt_from, dt_upto)
+        case "day":
+            return by_day_pipeline(dt_from, dt_upto)
         case _:
             raise NotImplementedError
